253c9e0416967456a379135184150f318bcb79de
sg3des <sg3des@gmail.com>
2018-03-04 17:00:44 +0300

    added file writer

--- logfilter.go
+++ logfilter.go
@@ -17,17 +17,42 @@ func NewFilter() *Filter {
 }
 
 type logWriter struct {
+	filename string
 	w        io.Writer
 	strict   bool
 	prefixes []LogPrefix
 }
 
 func (l *Filter) AddWriter(w io.Writer, prefixes ...LogPrefix) {
-	l.writers = append(l.writers, logWriter{w, false, prefixes})
+	l.writers = append(l.writers, logWriter{
+		w:        w,
+		strict:   false,
+		prefixes: prefixes,
+	})
 }
 
 func (l *Filter) AddStrictWriter(w io.Writer, prefixes ...LogPrefix) {
-	l.writers = append(l.writers, logWriter{w, true, prefixes})
+	l.writers = append(l.writers, logWriter{
+		w:        w,
+		strict:   true,
+		prefixes: prefixes,
+	})
+}
+
+func (l *Filter) AddFileWriter(filename string, prefixes ...LogPrefix) {
+	l.writers = append(l.writers, logWriter{
+		filename: filename,
+		strict:   false,
+		prefixes: prefixes,
+	})
+}
+
+func (l *Filter) AddStrictFileWriter(filename string, prefixes ...LogPrefix) {
+	l.writers = append(l.writers, logWriter{
+		filename: filename,
+		strict:   true,
+		prefixes: prefixes,
+	})
 }
 
 func (lw *logWriter) Check(p []byte, prefix LogPrefix) bool {
@@ -46,6 +71,18 @@ func (lw *logWriter) Check(p []byte, prefix LogPrefix) bool {
 	}
 
 	return false
+}
+
+func (lw *logWriter) Write(p []byte) (int, error) {
+	if lw.filename != "" && lw.w == nil {
+		var err error
+		lw.w, err = os.OpenFile(lw.filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	return lw.w.Write(p)
 }
 
 func (l *Filter) getPrefix(p []byte) LogPrefix {
@@ -69,7 +106,7 @@ func (l *Filter) Write(p []byte) (int, error) {
 
 	for _, lw := range l.writers {
 		if lw.Check(p, prefix) {
-			n, err := lw.w.Write(p)
+			n, err := lw.Write(p)
 			if err != nil {
 				return n, err
 			}
